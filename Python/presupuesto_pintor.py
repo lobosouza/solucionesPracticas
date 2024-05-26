5d1f13fe080f5e09cf331dc1ab60884b34184513
lobosouza <lobosouza.it@gmail.com>
2024-05-25 22:21:54 -0300

    update

--- Python/presupuesto_pintor.py
+++ Python/presupuesto_pintor.py
@@ -1,1 +1,16 @@
+''' Variables
 
+monto_fijo → float
+
+m2 → float 
+
+presupuesto → float
+
+Resultado
+
+PresupuestoTotal = monto_fijo * m2 '''
+
+monto_fijo = float(input("Ingrese monto fijo de la mano de obra: "))
+m2= float(input("Ingrese m2: "))
+PresupuestoTotal = monto_fijo * m2 
+print("El valor total del presupuesto es: ", PresupuestoTotal)
