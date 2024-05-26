a2105d81b93d9e4cd27b75175a9808dc3dda9a32
lobosouza <lobosouza.it@gmail.com>
2024-05-26 02:20:20 -0300

    update

--- Python/NombresPersonasIterativo.py
+++ Python/NombresPersonasIterativo.py
@@ -1,1 +1,12 @@
+'''
+Pedir que el usuario ingrese (input) nombre de personas y mostrarlos hasta que el valor 
+de lo que ingresa sea “fin” '''
+
+nombrepersonas = "Fin"
+
+nombrepersonas = input("Ingrese nombres de personas. Ingrese Fin si desea terminar. ")
+
+while nombrepersonas != "Fin":
+    nombrepersonas = input("Ingrese nombres de personas. Ingrese Fin si desea terminar ")
+
 
