c082826a3ff015688febf2af93928f2b77869248
lobosouza <lobosouza.it@gmail.com>
2024-05-26 03:15:26 -0300

    update

---
+++ Python/ListaUsuarios.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+usuarios = "Fin"
+
+usuarios = input("Ingrese nombre de usuario. Ingrese Fin si desea terminar. ")
+
+while usuarios != "Fin":
+    usuarios = input("Ingrese nombre de usuario. Ingrese Fin si desea terminar ")
+
+# como crear una lista con los usuarios ingresados?
+
+list = []
+print(list)
