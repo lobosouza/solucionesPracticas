a697a4851a866ba438d3216341e7a48e807f7094
lobosouza <lobosouza.it@gmail.com>
2024-05-26 02:02:45 -0300

    update

--- Python/NumerosParesIterativo.py
+++ Python/NumerosParesIterativo.py
@@ -1,1 +1,9 @@
+'''Mostrar sólo los números pares desde 0 hasta el número ingresado (input). 
+Nota: para saber si un número es par hacer i % 2 == 0)'''
 
+n = int(input("Ingrese un número: "))
+
+for i in range(n):
+    if i % 2 == 0:
+        print(i)
+    
